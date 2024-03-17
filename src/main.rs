4064d82ccae4e862880836238bc1e513b15d2d30
Tim <kristoft.1329@gmail.com>
2024-03-17 11:06:24 +0300

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::{
     io::{BufRead, BufReader, Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -29,15 +29,65 @@ fn main() {
                 let path = response[0].split(' ').nth(1);
 
                 match path {
-                    Some("/") => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").expect("Error"),
-                    _ => stream
-                        .write_all(b"HTTP/1.1 404 NOT_FOUND\r\n\r\n")
-                        .expect("Error"),
+                    Some(s) if s.starts_with("/echo/") => {
+                        let echo = &s[6..];
+                        let content_type = String::from("Content-type: text/plain");
+                        let content_len_header = format!("Content-length: {}", echo.len());
+                        Response::ok(
+                            vec![content_type, content_len_header],
+                            Some(echo.to_owned()),
+                        )
+                        .write_to(&mut stream);
+                    }
+                    Some("/") => Response::ok(vec![], None).write_to(&mut stream),
+                    _ => Response::not_found().write_to(&mut stream),
                 };
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+struct Response {
+    status_code: (usize, String),
+    headers: Vec<String>,
+    body: Option<String>,
+}
+
+impl Response {
+    fn as_string(&self) -> String {
+        let headers_str = self.headers.join("\r\n");
+        let status_str = format!("HTTP/1.1 {} {}", self.status_code.0, self.status_code.1);
+        let body_str = match &self.body {
+            Some(b) => b.as_str(),
+            None => "",
+        };
+
+        format!("{}\r\n{}\r\n\r\n{}", status_str, headers_str, body_str)
+    }
+
+    fn ok(headers: Vec<String>, body: Option<String>) -> Response {
+        Response {
+            status_code: (200, String::from("Ok")),
+            headers,
+            body,
+        }
+    }
+
+    fn not_found() -> Response {
+        Response {
+            status_code: (404, String::from("Not found")),
+            headers: vec![],
+            body: None,
+        }
+    }
+
+    fn write_to(&self, stream: &mut TcpStream) {
+        let binding = self.as_string();
+        let bytes = binding.as_bytes();
+
+        stream.write_all(bytes).expect("Failed")
     }
 }
