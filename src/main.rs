60883c6cfa5df181627e5ed4d3be6d538abbc534
Tim <kristoft.1329@gmail.com>
2024-03-19 00:12:21 +0300

    stage 8

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::{
     env, fs,
-    io::{BufRead, BufReader, Write},
+    io::{BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
     thread,
 };
@@ -45,22 +45,63 @@ fn read_file(dir: Option<String>, filename: &str) -> Option<String> {
     }
 }
 
+fn read_headers(buffer: &mut BufReader<&TcpStream>) -> String {
+    let mut data = String::new();
+
+    let mut line = String::new();
+    loop {
+        buffer.read_line(&mut line).unwrap_or_default();
+        if line.starts_with("\r\n") {
+            break;
+        }
+
+        data.push_str(&line);
+        line.clear();
+    }
+    data
+}
+
+fn read_body(buffer: &mut BufReader<&TcpStream>, len: usize) -> String {
+    let mut body_bytes = vec![0; len];
+
+    buffer.read_exact(&mut body_bytes).unwrap_or_default();
+
+    String::from_utf8(body_bytes).unwrap_or(String::new())
+}
+
 fn process(mut stream: TcpStream, dir: Option<String>) {
     println!("accepted new connection");
 
-    let buffer = BufReader::new(&stream);
+    let mut buffer = BufReader::new(&stream);
 
-    let response: Vec<_> = buffer
-        .lines()
-        .map(|s| s.unwrap())
-        .take_while(|s| !s.is_empty())
-        .take(3)
-        .collect();
+    let headers = read_headers(&mut buffer);
 
-    let path = response[0].split(' ').nth(1);
+    let response: Vec<_> = headers.split("\r\n").collect();
+
+    let first = response[0].split(' ').collect::<Vec<&str>>();
+    let method = first[0];
+    let path = first[1];
 
     match path {
-        Some(s) if s.starts_with("/files/") => {
+        s if s.starts_with("/files/") && method == "POST" => {
+            let content_len = response
+                .iter()
+                .find(|it| it.to_lowercase().contains("content-length"))
+                .map(|header| header.split(": ").nth(1).unwrap())
+                .map(|len| len.parse::<usize>().unwrap_or(0))
+                .unwrap_or(0);
+
+            let filename = &s[7..];
+            let body = read_body(&mut buffer, content_len);
+
+            let write_result = fs::write(filename, body);
+            if write_result.is_ok() {
+                Response::created().write_to(&mut stream);
+            } else {
+                Response::not_found().write_to(&mut stream);
+            }
+        }
+        s if s.starts_with("/files/") && method == "GET" => {
             let filename = &s[7..];
             let file_res = read_file(dir, filename);
 
@@ -75,7 +116,7 @@ fn process(mut stream: TcpStream, dir: Option<String>) {
                 None => Response::not_found().write_to(&mut stream),
             }
         }
-        Some(s) if s.starts_with("/echo/") => {
+        s if s.starts_with("/echo/") => {
             let echo = &s[6..];
             let content_type = String::from("Content-type: text/plain");
             let content_len_header = format!("Content-length: {}", echo.len());
@@ -85,7 +126,7 @@ fn process(mut stream: TcpStream, dir: Option<String>) {
             )
             .write_to(&mut stream);
         }
-        Some(s) if s.starts_with("/user-agent") => {
+        s if s.starts_with("/user-agent") => {
             let user_agent_res = response.iter().find(|s| s.starts_with("User-Agent:"));
 
             let user_agent = match user_agent_res {
@@ -101,7 +142,7 @@ fn process(mut stream: TcpStream, dir: Option<String>) {
             )
             .write_to(&mut stream);
         }
-        Some("/") => Response::ok(vec![], None).write_to(&mut stream),
+        "/" => Response::ok(vec![], None).write_to(&mut stream),
         _ => Response::not_found().write_to(&mut stream),
     };
 }
@@ -129,6 +170,14 @@ impl Response {
             status_code: (200, String::from("Ok")),
             headers,
             body,
+        }
+    }
+
+    fn created() -> Self {
+        Response {
+            status_code: (201, String::from("Created")),
+            headers: vec![],
+            body: None,
         }
     }
 
